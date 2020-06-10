f120db5c931fae0e58099053b21052e03017ee99
goldwinB <goldwin@africanpg.co.za>
2020-06-10 17:37:35 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,223 @@
+(function($) {
+  "use strict";
+
+  /*--------------------------
+  preloader
+  ---------------------------- */
+  $(window).on('load', function() {
+    var pre_loader = $('#preloader');
+    pre_loader.fadeOut('slow', function() {
+      $(this).remove();
+    });
+  });
+
+  /*---------------------
+   TOP Menu Stick
+  --------------------- */
+  var s = $("#sticker");
+  var pos = s.position();
+  $(window).on('scroll', function() {
+    var windowpos = $(window).scrollTop() > 300;
+    if (windowpos > pos.top) {
+      s.addClass("stick");
+    } else {
+      s.removeClass("stick");
+    }
+  });
+
+  /*----------------------------
+   Navbar nav
+  ------------------------------ */
+  var main_menu = $(".main-menu ul.navbar-nav li ");
+  main_menu.on('click', function() {
+    main_menu.removeClass("active");
+    $(this).addClass("active");
+  });
+
+  /*----------------------------
+   wow js active
+  ------------------------------ */
+  new WOW().init();
+
+  $(".navbar-collapse a:not(.dropdown-toggle)").on('click', function() {
+    $(".navbar-collapse.collapse").removeClass('in');
+  });
+
+  //---------------------------------------------
+  //Nivo slider
+  //---------------------------------------------
+  $('#ensign-nivoslider').nivoSlider({
+    effect: 'random',
+    slices: 15,
+    boxCols: 12,
+    boxRows: 8,
+    animSpeed: 500,
+    pauseTime: 5000,
+    startSlide: 0,
+    directionNav: true,
+    controlNavThumbs: false,
+    pauseOnHover: true,
+    manualAdvance: false,
+  });
+
+  /*----------------------------
+   Scrollspy js
+  ------------------------------ */
+  var Body = $('body');
+  Body.scrollspy({
+    target: '.navbar-collapse',
+    offset: 80
+  });
+
+  /*---------------------
+    Venobox
+  --------------------- */
+  var veno_box = $('.venobox');
+  veno_box.venobox();
+
+  /*----------------------------
+  Page Scroll
+  ------------------------------ */
+  var page_scroll = $('a.page-scroll');
+  page_scroll.on('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top - 55
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
+
+  /*--------------------------
+    Back to top button
+  ---------------------------- */
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+
+  /*----------------------------
+   Parallax
+  ------------------------------ */
+  var well_lax = $('.wellcome-area');
+  well_lax.parallax("50%", 0.4);
+  var well_text = $('.wellcome-text');
+  well_text.parallax("50%", 0.6);
+
+  /*--------------------------
+   collapse
+  ---------------------------- */
+  var panel_test = $('.panel-heading a');
+  panel_test.on('click', function() {
+    panel_test.removeClass('active');
+    $(this).addClass('active');
+  });
+
+  /*---------------------
+   Testimonial carousel
+  ---------------------*/
+  var test_carousel = $('.testimonial-carousel');
+  test_carousel.owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+  /*----------------------------
+   isotope active
+  ------------------------------ */
+//  $('.nivo-slice').on('load', function(e){
+ 
+//     var src = $('img.nivo-main-image').attr('src');
+    
+//     if(src == "images/slides/slide-money-heists.png")
+//     {
+//       document.querySelector('.slide-p').innerHTML= "Money Heist (2020)";
+//     }
+//     else if(src == "images/slides/slide-see.jpg")
+//     {
+//       document.querySelector('.slide-p').innerHTML= "See (2020)";
+      
+//     }
+//     else if(src == "images/slides/slide-extraction.jpg")
+//     {
+//       document.querySelector('.slide-p').innerHTML= "Extraction (2020)";
+//     }
+//     else if(src == "images/slides/slide-bloodshot.jpg")
+//     {
+//       document.querySelector('.slide-p').innerHTML= "Bloodshot (2020)";
+//     }
+//     else if(src == "images/slides/slide-badlands.jpg")
+//     {
+//       document.querySelector('.slide-p').innerHTML= "Into The Badlands (2020)";
+//     }
+//     else
+//     {
+//       document.querySelector('.slide-p').innerHTML= "";
+//     }
+//  });
+
+//  $('nivo-main-image').trigger('changed', function(e){
+//   console.log('chnaged');
+//  });
+// console.log(textContent );
+// var dd = document.querySelector('.nivo-main-image').getAttribute('src');
+//console.log(dd);
+
+// document.querySelector('.nivo-main-image').getAttribute('src').addEventListener('load', function(e){
+//   console.log('chnaged');
+//  });
+
+  /*---------------------
+   Circular Bars - Knob
+--------------------- */
+  if (typeof($.fn.knob) != 'undefined') {
+    var knob_tex = $('.knob');
+    knob_tex.each(function() {
+      var $this = $(this),
+        knobVal = $this.attr('data-rel');
+
+      $this.knob({
+        'draw': function() {
+          $(this.i).val(this.cv + '%')
+        }
+      });
+
+      $this.appear(function() {
+        $({
+          value: 0
+        }).animate({
+          value: knobVal
+        }, {
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.val(Math.ceil(this.value)).trigger('change');
+          }
+        });
+      }, {
+        accX: 0,
+        accY: -150
+      });
+    });
+  }
+
+})(jQuery);
---
+++ js/toggle.js
@@ -0,0 +1,99 @@
+// $('.customs').on('click', function(e){
+
+//   $(this).find('i').toggleClass('fa-angle-right fa-angle-down');
+  
+// });
+
+function openNav() {
+  document.querySelector(".menu-toogle").style.display = "grid";
+
+  var iOS =  
+      /iPad|iPhone|iPod/.test(navigator.userAgent) && 
+      !window.MSStream;
+
+  if(iOS ==true)
+  {
+    document.getElementById("sticker_container").style.marginBottom = "200%";
+  }
+
+}
+
+function closeNav() {
+ 
+  //document.querySelector(".menu-toogle").style.height = "0%";
+  $('.menu-toogle').fadeOut(600);
+  document.getElementById("sticker_container").style.marginBottom = "0%";
+}
+
+
+$('img.nivo-main-image').on('load', function(e){
+ 
+  var src = $('img.nivo-main-image').attr('src');
+  
+  if(src == "images/slides/slide-money-heists.png")
+  {
+    document.querySelector('.slide-p').innerHTML= "Money Heist (2020)";
+  }
+  else if(src == "images/slides/slide-see.jpg")
+  {
+    document.querySelector('.slide-p').innerHTML= "See (2020)";
+    
+  }
+  else if(src == "images/slides/slide-extraction.jpg")
+  {
+    document.querySelector('.slide-p').innerHTML= "Extraction (2020)";
+  }
+  else if(src == "images/slides/slide-bloodshot.jpg")
+  {
+    document.querySelector('.slide-p').innerHTML= "Bloodshot (2020)";
+  }
+  else if(src == "images/slides/slide-badlands.jpg")
+  {
+    document.querySelector('.slide-p').innerHTML= "Into The Badlands (2020)";
+  }
+  else if(src == "images/slides/slide-queen-sono.jpg")
+  {
+    document.querySelector('.slide-p').innerHTML= "Queen Sono (2020)";
+  }
+  else if(src == "images/slides/blood&water.jpeg")
+  {
+    document.querySelector('.slide-p').innerHTML= "Blood And Water (2020)";
+  }
+  else
+  {
+    document.querySelector('.slide-p').innerHTML= "";
+  }
+});
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.intro');
+var textMessage = document.querySelector('.message-intro');
+textWrapper.innerHTML ="<p>WELCOM TO Funner_Movies</p>";
+textMessage.innerHTML ="<p>Watch And Download Your Favourate Movies And Series. Enjoy !!!</p>";
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.intro .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 6200,
+    delay: (el, i) => 50 + 30 * i
+  }).add({
+    targets: '.intro .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+  
+function close() {
+  $('#myModal').delay(10000).fadeOut(800);
+  
+}
