358d6960612752ef50c1f891a0658c5673da0216
goldwinB <goldwin@africanpg.co.za>
2020-06-10 23:34:20 +0100

    movies

---
+++ movies/main.js
@@ -0,0 +1,17 @@
+function playVideo(){
+
+  document.getElementById('video-cover').style.display = "none";
+  document.getElementById('video-cover-play').style.display = "block";
+}
+
+function download(){
+
+  document.getElementById('download-video').click();
+}
+
+function downloadSub(){
+
+  document.getElementById('download-sub').click();
+}
+
+
